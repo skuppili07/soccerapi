4479c498c3b449dfd6af18c6ccc40a74090620d7
surya_kuppili0730 <surya.kuppili94@gmail.com>
2018-05-31 20:29:04 -0500

    Setting up the code base

---
+++ src/main/java/com/fifa/soccer/springboot/soccerapi/SoccerapiApplication.java
@@ -0,0 +1,12 @@
+package com.fifa.soccer.springboot.soccerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoccerapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoccerapiApplication.class, args);
+	}
+}
